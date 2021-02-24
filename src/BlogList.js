548b72810b8d28d7cc8c68a389cf5bc631ea019a
amit-deshwal <a3deswal@gmail.com>
2021-02-25 03:47:00 +0530

    Added dummy data to be used to fetch data with help of json-server package & handled errors

---
+++ src/BlogList.js
@@ -0,0 +1,14 @@
+const BlogList = ({blogs}) => {
+    return (
+        <div className='blog-list'>
+            {blogs.map((item) => (
+                <div className='blog-preview' key={item.id}>
+                    <h2>{item.title}</h2>
+                    <p>Written by {item.author}</p>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default BlogList;
--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,36 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Bloglist from "./BlogList";
 
 const Home = () => {
+    const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [error,setErrorMessage] = useState(null);
+
+    useEffect(() => {
+        fetch("http://localhost:8000/blogs")
+            .then(res => {
+                console.log(res);
+                if(!res.ok){
+                    throw Error('Unable to load data from the resource')
+                }
+                return res.json();
+            })
+            .then(data => {
+                setBlogs(data);
+                setIsPending(false);
+                setErrorMessage(null);
+            })
+            .catch(err => {
+                setIsPending(false);
+                setErrorMessage(err.message);
+            })
+    }, []);
+
     return (
         <div className="home">
-            <h1>Homepage</h1>
+            {error && <div>{error} </div>}
+            {isPending && <div>Loading...</div>}
+            {blogs && <Bloglist blogs={blogs} />}
         </div>
     );
 };
