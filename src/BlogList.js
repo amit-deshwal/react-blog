f26dfd3510a58eeea3fc835f55f7297c63bc5004
amit-deshwal <a3deswal@gmail.com>
2021-03-02 02:00:57 +0530

    Render Blog by using ID

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,10 +1,14 @@
-const BlogList = ({blogs}) => {
+import { Link } from "react-router-dom";
+
+const BlogList = ({ blogs }) => {
     return (
         <div className='blog-list'>
             {blogs.map((item) => (
                 <div className='blog-preview' key={item.id}>
-                    <h2>{item.title}</h2>
-                    <p>Written by {item.author}</p>
+                    <Link to={`/blogs/${item.id}`}>
+                        <h2>{item.title}</h2>
+                        <p>Written by {item.author}</p>
+                    </Link>
                 </div>
             ))}
         </div>
